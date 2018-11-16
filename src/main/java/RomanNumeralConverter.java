9e193f06ab376919450fa0c5d5a7573ed1a91154
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-16 09:57:31 +0000

    Refactor: statement -> tail recursion

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -10,14 +10,13 @@ public class RomanNumeralConverter {
         results.put(1, "I");
         results.put(4, "IV");
         results.put(5, "V");
-        results.put(6, "VI");
-        results.put(7, "VII");
-        results.put(8, "VIII");
     }
 
     public String convert(int decimal) {
         if (results.containsKey(decimal))
             return results.get(decimal);
+        if (decimal > 5)
+            return results.get(5) + convert(decimal - 5);
         return results.get(1) + convert(decimal - 1);
     }
 }
