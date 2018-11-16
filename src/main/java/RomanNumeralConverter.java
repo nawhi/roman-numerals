b2b8dfabc2e707f149dbf33d7bcbb581885fb2ce
nawhi <25394965+nawhi@users.noreply.github.com>
2018-11-16 09:51:06 +0000

    Refactor: array -> collection

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,8 +1,20 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class RomanNumeralConverter {
 
-    private final String[] results = { "I", "II", "III", "IV" };
+    private final SortedMap<Integer, String> results;
+
+    public RomanNumeralConverter() {
+        results = new TreeMap<>();
+        results.put(1, "I");
+        results.put(2, "II");
+        results.put(3, "III");
+        results.put(4, "IV");
+    }
+
 
     public String convert(int decimal) {
-        return results[decimal - 1];
+        return results.get(decimal);
     }
 }
